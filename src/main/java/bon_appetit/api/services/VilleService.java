da7ebed03f5f006c57c47d125b5e00e8d5758b89
Thomas Grandjean <thomasgrandjean45@gmail.com>
2025-01-13 12:08:18 +0100

    minor change on VilleSerice

--- src/main/java/bon_appetit/api/services/VilleService.java
+++ src/main/java/bon_appetit/api/services/VilleService.java
@@ -1,10 +1,6 @@
 package bon_appetit.api.services;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import bon_appetit.api.models.Utilisateur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
