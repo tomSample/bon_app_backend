e58ebeebd1c06d33e3ae133bf594dc809812ea7d
Yannick <pro@ygras.fr>
2025-01-14 10:19:04 +0100

    Article configuration

---
+++ src/main/java/bon_appetit/api/repositories/ArticleRepository.java
@@ -0,0 +1,9 @@
+package bon_appetit.api.repositories;
+
+import bon_appetit.api.models.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+}
